0846d452ae01b9aff33fb50c887a48cbbe483b56
Michael Hall <michael@mbh.sh>
2021-05-12 15:31:19 +1000

    add filter status tags method

--- src/filter.rs
+++ src/filter.rs
@@ -8,6 +8,8 @@ pub enum TagsError {
     #[error("Unknown tag given {0}")]
     UnknownTag(String),
 }
+
+/// A collection of known VCF tags/fields/filters
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 enum Tags {
     FwdCovg,
@@ -27,6 +29,7 @@ enum Tags {
 }
 
 impl Tags {
+    /// Returns the string representation of the tag
     fn value(&self) -> &str {
         match *self {
             Tags::FwdCovg => "MED_FWD_COVG",
@@ -71,6 +74,59 @@ impl FromStr for Tags {
     }
 }
 
+struct FilterStatus {
+    low_covg: bool,
+    high_covg: bool,
+    strand_bias: bool,
+    low_gt_conf: bool,
+    high_gaps: bool,
+    long_indel: bool,
+    low_support: bool,
+}
+
+impl FilterStatus {
+    fn new() -> Self {
+        FilterStatus {
+            low_covg: false,
+            high_covg: false,
+            strand_bias: false,
+            low_gt_conf: false,
+            high_gaps: false,
+            long_indel: false,
+            low_support: false,
+        }
+    }
+    /// Returns the Tags for the filters this object fails
+    fn tags(&self) -> Vec<Tags> {
+        let mut tags = vec![];
+        if self.low_covg {
+            tags.push(Tags::LowCovg);
+        }
+        if self.high_covg {
+            tags.push(Tags::HighCovg);
+        }
+        if self.low_gt_conf {
+            tags.push(Tags::LowGtConf);
+        }
+        if self.strand_bias {
+            tags.push(Tags::StrandBias);
+        }
+        if self.high_gaps {
+            tags.push(Tags::HighGaps);
+        }
+        if self.long_indel {
+            tags.push(Tags::LongIndel);
+        }
+        if self.low_support {
+            tags.push(Tags::LowSupport);
+        }
+        if tags.is_empty() {
+            tags.push(Tags::Pass);
+        }
+        tags
+    }
+}
+
 pub trait Filter {}
 
 #[cfg(test)]
@@ -98,6 +154,64 @@ mod test {
 
         let actual = Tags::from_str(s);
         let expected = Err(TagsError::UnknownTag(s.to_string()));
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn filter_status_tags_none_fail() {
+        let status = FilterStatus::new();
+
+        let actual = status.tags();
+        let expected = &[Tags::Pass];
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn filter_status_tags_all_fail() {
+        let status = FilterStatus {
+            low_covg: true,
+            high_covg: true,
+            strand_bias: true,
+            low_gt_conf: true,
+            high_gaps: true,
+            long_indel: true,
+            low_support: true,
+        };
+
+        let mut actual = status.tags();
+        actual.sort_unstable();
+        let mut expected = vec![
+            Tags::LowCovg,
+            Tags::HighCovg,
+            Tags::LowGtConf,
+            Tags::StrandBias,
+            Tags::HighGaps,
+            Tags::LongIndel,
+            Tags::LowSupport,
+        ];
+        expected.sort_unstable();
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn filter_status_tags_some_fail() {
+        let status = FilterStatus {
+            low_covg: false,
+            high_covg: false,
+            strand_bias: true,
+            low_gt_conf: false,
+            high_gaps: true,
+            long_indel: false,
+            low_support: false,
+        };
+
+        let mut actual = status.tags();
+        actual.sort_unstable();
+        let mut expected = vec![Tags::StrandBias, Tags::HighGaps];
+        expected.sort_unstable();
 
         assert_eq!(actual, expected)
     }
