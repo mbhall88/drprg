42466febbf49d7fd595871440db454f04e9c9c23
Michael Hall <michael@mbh.sh>
2022-11-22 12:00:52 +1000

    Update combine_mykrobe_reports.py

--- paper/workflow/scripts/combine_mykrobe_reports.py
+++ paper/workflow/scripts/combine_mykrobe_reports.py
@@ -1,11 +1,14 @@
 import sys
+from itertools import chain
 
 sys.stderr = open(snakemake.log[0], "w")
 
 import json
 import gzip
 from pathlib import Path
+from multiprocessing import Pool
 
+N = 100  # number of files to pass to a process
 
 if str(snakemake.output.report).endswith("csv"):
     DELIM = ","
@@ -28,50 +31,70 @@ def load_susceptibility(stream) -> dict:
         return data["susceptibility"]
 
 
-with open(snakemake.output.report, "w") as fout:
+def load_report(p: Path) -> list[str]:
+    proj = p.parts[-3]
+    sample = p.parts[-2]
+    run = p.name.split(".")[0]
+    tech = snakemake.wildcards.tech
 
-    print(
-        DELIM.join(
-            [
-                "run",
-                "biosample",
-                "bioproject",
-                "technology",
-                "tool",
-                "drug",
-                "prediction",
-            ]
-        ),
-        file=fout,
-    )
+    fopen = gzip.open if p.suffix == ".gz" else open
 
-    for p in map(Path, snakemake.input.reports):
-        proj = p.parts[-3]
-        sample = p.parts[-2]
-        run = p.name.split(".")[0]
-        tech = snakemake.wildcards.tech
+    with fopen(p) as fp:
+        report = load_susceptibility(fp)
 
-        fopen = gzip.open if p.suffix == ".gz" else open
+    if not report:
+        raise ValueError(f"{run} has no susceptibility results")
 
-        with fopen(p) as fp:
-            report = load_susceptibility(fp)
+    rows = []
 
-        if not report:
-            eprint(f"[WARNING] {run} has no susceptibility results")
-            continue
-
-        for drug, pred in report.items():
-            print(
-                DELIM.join(
-                    (
-                        run,
-                        sample,
-                        proj,
-                        tech,
-                        "mykrobe",
-                        drug,
-                        pred["predict"],
-                    )
-                ),
-                file=fout,
+    for drug, pred in report.items():
+        row = DELIM.join(
+            (
+                run,
+                sample,
+                proj,
+                tech,
+                "mykrobe",
+                drug,
+                pred["predict"],
             )
+        )
+        rows.append(row)
+
+    return rows
+
+
+def flatten(list_of_lists):
+    """Flatten one level of nesting"""
+    return chain.from_iterable(list_of_lists)
+
+
+def main():
+    with open(snakemake.output.report, "w") as fout:
+
+        print(
+            DELIM.join(
+                [
+                    "run",
+                    "biosample",
+                    "bioproject",
+                    "technology",
+                    "tool",
+                    "drug",
+                    "prediction",
+                ]
+            ),
+            file=fout,
+        )
+
+        mykrobe_reports = list(map(Path, snakemake.input.reports))
+
+        with Pool(snakemake.threads) as pool:
+            rows = pool.map(load_report, mykrobe_reports, chunksize=N)
+
+        for row in flatten(rows):
+            print(row, file=fout)
+
+
+if __name__ == "__main__":
+    main()
