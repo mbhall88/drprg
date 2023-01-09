2a7374cc613ad142c85cbcca978fc7d11cb0efa4
Michael Hall <michael@mbh.sh>
2023-01-09 10:28:01 +1000

    check snakemake is on PATH

--- paper/workflow/scripts/submit_lsf.sh
+++ paper/workflow/scripts/submit_lsf.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -eux
 
+if ! command -v snakemake &>/dev/null; then
+  echo "snakemake could not be found"
+  exit 1
+fi
+
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 LOG_DIR=logs/
 
 if [[ ! -d "$LOG_DIR" ]]; then
-    echo "Error: Log directory $LOG_DIR does not exist"
-    exit 1
+  echo "Error: Log directory $LOG_DIR does not exist"
+  exit 1
 fi
 
 MEMORY=4000
@@ -21,14 +26,14 @@ SNAKEMAKE_TMP="tmp/snakemake"
 mkdir -p "$SNAKEMAKE_TMP"
 
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
-    -M "$MEMORY" \
-    -n "$THREADS" \
-    -o "$LOG_DIR"/"$JOB_NAME".o \
-    -e "$LOG_DIR"/"$JOB_NAME".e \
-    -J "$JOB_NAME" \
-    snakemake --profile "$PROFILE" \
-    --default-resources tmpdir=$SNAKEMAKE_TMP \
-    --local-cores "$THREADS" \
-    "$@" --singularity-args "$ARGS"
+  -M "$MEMORY" \
+  -n "$THREADS" \
+  -o "$LOG_DIR"/"$JOB_NAME".o \
+  -e "$LOG_DIR"/"$JOB_NAME".e \
+  -J "$JOB_NAME" \
+  snakemake --profile "$PROFILE" \
+  --default-resources tmpdir=$SNAKEMAKE_TMP \
+  --local-cores "$THREADS" \
+  "$@" --singularity-args "$ARGS"
 
 exit 0
